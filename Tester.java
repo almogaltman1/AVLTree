8f6cb453757a88c6e032af9a86789132cfc1babc
danimjaffe <danieljaffe@mail.tau.ac.il>
2021-11-15 12:43:12 +0200

    1. add printTree function in Tester

--- Tester.java
+++ Tester.java
@@ -1,9 +1,41 @@
 public class Tester {
     public static void main (String[]args){
         AVLTree t = new AVLTree();
-        AVLTree.AVLNode root = t.new AVLNode(10,"11",null);
-        t.minNode=root;
-        System.out.println(t.min());
+        AVLTree.AVLNode root = t.new AVLNode(10,"root",null);
+        printTree(root);
+
+    }
+
+    static final int COUNT = 10;
+
+    static void printTreeUtil(AVLTree.IAVLNode root, int space)
+    {
+        // Base case
+        if (root == null)
+            return;
+
+        // Increase distance between levels
+        space += COUNT;
+
+        // Process right child first
+        printTreeUtil(root.getRight(), space);
+
+        // Print current node after space
+        // count
+        System.out.print("\n");
+        for (int i = COUNT; i < space; i++)
+            System.out.print(" ");
+        System.out.print(root.getKey() + "\n");
+
+        // Process left child
+        printTreeUtil(root.getLeft(), space);
+    }
+
+    // Wrapper over print2DUtil()
+    static void printTree(AVLTree.IAVLNode root)
+    {
+        // Pass initial space count as 0
+        printTreeUtil(root, 0);
     }
 
 }
